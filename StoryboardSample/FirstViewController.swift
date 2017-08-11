14e3676128e69070ca791aa4f0e8efe712d1f383
yoizzing <renasul@daum.net>
2017-08-11 16:16:54 +0900

    #1 새로운 파일도 추가했음

---
+++ StoryboardSample/FirstViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstViewController.swift
+//  StoryboardSample
+//
+//  Created by ktds 06 on 2017. 8. 11..
+//  Copyright © 2017년 CJOliveNetworks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
