047178dc0e6f7c37b185bbfb97307464ff2ab328
yoizzing <renasul@daum.net>
2017-08-11 17:43:16 +0900

    #3 seque 코드로 업로드

---
+++ StoryboardSample/BlueViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BlueViewController.swift
+//  StoryboardSample
+//
+//  Created by ktds 06 on 2017. 8. 11..
+//  Copyright © 2017년 CJOliveNetworks. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    @IBAction func close(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- StoryboardSample/ViewController.swift
+++ StoryboardSample/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func openAD(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "bluescene")
+    
+        
+        if let newVC = vc{
+            self.modalPresentationStyle = .popover
+            self.present(newVC, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
